002b554e9f1c43b8a3ed86e74f4b0568378db86a
Mario Nguyễn <53334967+marionguyen20@users.noreply.github.com>
2019-11-28 00:50:04 +0700

    Draw Board

--- Tetris/src/Board.java
+++ Tetris/src/Board.java
@@ -1,3 +1,26 @@
-public class Board {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Board extends JPanel {
+
+    private BufferedImage blocks;
+
+    public Board () {
+        try {
+            blocks = ImageIO.read(Board.class.getResource("/Blocks.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.drawImage(blocks,0,0,null);
+
+    }
    
 }
