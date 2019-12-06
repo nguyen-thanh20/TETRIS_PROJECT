3b712d6b74e96d4aa0dfe033745d786c508752ff
sonicthinh2000 <58232543+sonicthinh2000@users.noreply.github.com>
2019-12-07 00:22:00 +0700

    update rotate

--- Tetris/src/Shape.java
+++ Tetris/src/Shape.java
@@ -68,7 +68,7 @@ public class Shape {
         int[][] newMatrix = new int[matrix[0].length][matrix.length];
 
         for(int i = 0; i < matrix.length; i++)
-            for(int j = 0; j < matrix[0].length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
                 newMatrix[j][i] = matrix[i][j];
             }
 
